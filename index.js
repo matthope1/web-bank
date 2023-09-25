ffeb3087cf8d76ad4f673af75946f632ef704d37
matt <matt-hope@hotmail.com>
2023-09-25 16:51:22 -0400

    removed handlebars & added express-handlebars + started creating views

--- index.js
+++ index.js
@@ -1,9 +1,42 @@
 const express = require('express')
+const exphbs = require('express-handlebars');
+const path = require("path");
 const app = express()
 const port = 3000
 
+app.engine(".hbs", exphbs.engine({											
+    extname: ".hbs",                                                 
+    defaultLayout: false,                                              
+    layoutsDir: path.join(__dirname, "/views")                           
+}));
+
+app.set("view engine", ".hbs");   
+
 app.get('/', (req, res) => {
   res.send(`It's alive... It's alive, it's moving, it's alive, it's alive, it's alive, it's alive, IT'S ALIVE!`)
+})
+app.get("/bank", (req,res) => {
+    
+    var bankData = {
+				bankName: "Club Cyberia Bank",
+        timeOpen: {
+            startTime: "9:30AM",
+            finishTime: "12:30PM",
+        },
+        myList: ["item1", "item2", "item3"],
+        location: {
+            city: "Toronto",
+        },
+    };
+
+    res.render('viewData', {                                               
+        data: bankData 
+    });
+        
+});
+
+app.get('*', (req, res) => {
+    res.send('This page cannot be found (^_^)')
 })
 
 app.listen(port, () => {
