b2d476ab027806bdb3221ae32b2dfe9051d7f0e1
matt <matt-hope@hotmail.com>
2023-10-30 19:44:39 -0400

    condense login logic

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cookieParser = require("cookie-parser");
 const exphbs = require('express-handlebars')
 const path = require('path')
 const fs = require('fs')
@@ -8,6 +9,7 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser());
 
 app.engine(".hbs", exphbs.engine({											
     extname: ".hbs",                                                 
@@ -23,6 +25,9 @@ app.get('/', (req, res) => {
 
 app.get("/login", (req,res) => {
     
+    const { cookies } = req.cookies
+    console.log("login get cookies", cookies)
+
     var bankData = {
         bankName: "Club Cyberia Bank",
         timeOpen: {
@@ -43,41 +48,48 @@ app.get("/login", (req,res) => {
 
 app.post('/login', (req, res) => {
     const {username, password} = req.body
+    const { cookies } = req.cookies
     const {passValid, msg} = validatePassword(username, password)
     // console.log({passValid, msg})
     // if its not valid, redirect to login page with err message
-    if (!passValid) {
-        res.render('loginPage', {
-            data: {
-                msg: msg,
-                bankName: "Club Cyberia Bank",
-                timeOpen: {
-                    startTime: "9:30AM",
-                    finishTime: "12:30PM",
-                },
-                myList: ["item1", "item2", "item3"],
-                location: {
-                    city: "Toronto",
-                },
-            }
-        })
-    } else {
-        res.render('bankingPage', {
-            data: {
-                msg: msg,
-                username: username,
-                bankName: "Club Cyberia Bank",
-                timeOpen: {
-                    startTime: "9:30AM",
-                    finishTime: "12:30PM",
-                },
-                myList: ["item1", "item2", "item3"],
-                location: {
-                    city: "Toronto",
-                },
-            }
-        })
+
+    // sending a cookie
+    // console.log("Cookies from client: ", req.cookies);                              //  Displays cookies submitted via request message
+    // res.cookie("firstName", "Albert");                                              //  Set cookie name as "firstName" and value as "Albert"
+    // res.send("<h0>Send one cookie from server to client</h1>");
+    // end sending a cookie
+
+    // setting a cookie that expires
+    // res.cookie("expiration", "ten", {maxAge: 9999});                               //  Set cookie name as "expiration" and value as "ten" and expire in 10000 miliseconds
+    //  res.cookie(name, 'value', {expire: 359999 + Date.now()});                   //  Expires after 360000 ms from the time the cookie is set
+    // end setting a cookie that expires
+
+    //  getting cookie from request
+    // console.log("Cookies from client: ", req.cookies);   
+    //  end getting cookie from request
+
+    console.log("login post cookies", cookies)
+
+    let page = ''
+    let data = {
+        msg: msg,
+        username: username,
+        bankName: "Club Cyberia Bank",
+        timeOpen: {
+            startTime: "9:30AM",
+            finishTime: "12:30PM",
+        },
+        myList: ["item1", "item2", "item3"],
+        location: {
+            city: "Toronto",
+        },
     }
+
+    page = passValid ? 'bankingPage' : 'loginPage'
+    console.log("setting username cookie...")
+    res.cookie("username", username);
+    res.render(page, {data})
+
 })
 
 
